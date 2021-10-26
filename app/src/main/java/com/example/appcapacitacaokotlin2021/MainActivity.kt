95c6e71fa1a32b2a639b9eb0bfa215be3e6315e6
Matheus Galbero Zanetoni <matheus_zanetoni@outlook.com.br>
2021-10-26 18:29:49 -0300

    Início da programação do algoritmo de cálculo da fórmula de Bhaskara e ajustes

--- app/src/main/java/com/example/appcapacitacaokotlin2021/MainActivity.kt
+++ app/src/main/java/com/example/appcapacitacaokotlin2021/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.appcapacitacaokotlin2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editTxtValorA: EditText
+    lateinit var editTxtValorB: EditText
+    lateinit var editTxtValorC: EditText
+    lateinit var btnCalcular: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editTxtValorA = findViewById(R.id.editTxtValorA)
+        editTxtValorB = findViewById(R.id.editTxtValorB)
+        editTxtValorC = findViewById(R.id.editTxtValorC)
+        btnCalcular = findViewById(R.id.btnCalcular)
+
+        btnCalcular.setOnClickListener() {
+            var delta = calcularDelta(editTxtValorA.text.toString().toDouble(), editTxtValorB.text.toString().toDouble(), editTxtValorC.text.toString().toDouble())
+            if (delta < 0) {
+                Toast.makeText(applicationContext, "O valor de delta é " + delta + ". A equação não possui raízes reais.", Toast.LENGTH_LONG).show()
+            } else {
+                //Cálculo das raízes
+            }
+        }
+    }
+
+    private fun calcularDelta(a: Double, b: Double, c: Double) : Double {
+        return Math.pow(b, 2.0) - 4 * a * c
     }
 }
